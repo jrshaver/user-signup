d7d7804a652deaf7910a1ad9847e25064888f142
Justin Shaver <justinrshaver@gmail.com>
2017-09-25 13:56:11 -0500

    Mission complete

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 from flask import Flask, request, redirect, render_template
-import html
+import html 
 import os
 
 app=Flask(__name__)
 app.config['DEBUG']=True
 
-username_err=""
-password_err=""
-vpassword_err=""
-email_err=""
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/", methods=["POST"])
-def index():
+def welcome():
+    
+    username_err=""
+    password_err=""
+    vpassword_err=""
+    email_err=""
 
     #fetch userdata from form
-    username=request.form["username"]
-    password=request.form["password"]
-    vpassword=request.form["vpassword"]
-    email=request.form["email"]
+    username=html.escape(request.form["username"])
+    password=html.escape(request.form["password"])
+    vpassword=html.escape(request.form["vpassword"])
+    email=html.escape(request.form["email"])
 
     #check username
     if not username or " " in username or len(username)<3 or len(username)>20:
         username_err="Please enter a valid username."
-        return render_template("index.html",username_err=username_err)
 
     #check password
-    elif not password or " " in password or len(password)<3 or len(password)>20:
-        username_err="Please enter a valid username."
-        return render_template("index.html",password_err=password_err)
+    if not password or " " in password or len(password)<3 or len(password)>20:
+        password_err="Please enter a valid password."
     
     #check vpassword
-    elif vpassword!=password:
+    if vpassword!=password:
         vpassword_err="Your passwords don't match."
-        return render_template("index.html",vpassword_err=vpassword_err)
 
     #check email
-    elif email.count("@")!=1 or email.count(".")!=1:
-        email_err="Please enter a valid email address or leave blank."
-        return render_template("index.html",email_err=email_err)
+    if email:
+        if email.count("@")!=1 or email.count(".")!=1:
+            email_err="Please enter a valid email address or leave blank."
 
+    if not username_err and not password_err and not vpassword_err:
+        return render_template("/welcome.html",username=username)
     else:
-        return redirect("/welcome.html",username=username)
+        return render_template("index.html", username=username,email=email,username_err=username_err,password_err=password_err,vpassword_err=vpassword_err,email_err=email_err)
     
 
 app.run()
